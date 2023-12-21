1766671aa253ea42fc5c9a00261493ada26c109d
VlackoDalze <51289587+BladiCreator@users.noreply.github.com>
2023-12-21 23:44:01 +0100

    Mejoras en el responsive

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -17,17 +17,17 @@ export class SkillsComponent {
     {name: "CSS3",logoLink: "assets/icons/skills/css_icon.svg"},
     {name: "JavaScript",logoLink: "assets/icons/skills/javascript_icon.svg"},
     {name: "TypeScript",logoLink: "assets/icons/skills/typescript_icon.svg"},
-    {name: "Java",logoLink: "assets/icons/skills/java_icon.svg"},
+    {name: "Bootstrap",logoLink: "assets/icons/skills/bootstrap_icon.svg"},
     {name: "C#",logoLink: "assets/icons/skills/csharp_icon.svg"},
     {name: "Python",logoLink: "assets/icons/skills/python_icon.svg"},
+    {name: "Java",logoLink: "assets/icons/skills/java_icon.svg"},
+    {name: "Android Studio",logoLink: "assets/icons/skills/android_studio_icon.svg"},
+    {name: "Dart",logoLink: "assets/icons/skills/dart_icon.svg"},
+    {name: "Flutter",logoLink: "assets/icons/skills/flutter_icon.svg"},
+    {name: "MySQL",logoLink: "assets/icons/skills/mysql_icon.svg"},
     {name: "Git",logoLink: "assets/icons/skills/git_icon.svg"},
     {name: "GitHub",logoLink: "assets/icons/skills/github_icon.svg"},
     {name: "XML",logoLink: "assets/icons/skills/xml_icon.svg"},
-    {name: "MySQL",logoLink: "assets/icons/skills/mysql_icon.svg"},
-    {name: "Flutter",logoLink: "assets/icons/skills/flutter_icon.svg"},
-    {name: "Dart",logoLink: "assets/icons/skills/dart_icon.svg"},
-    {name: "Android Studio",logoLink: "assets/icons/skills/android_studio_icon.svg"},
-    {name: "Bootstrap",logoLink: "assets/icons/skills/bootstrap_icon.svg"},
   ];
 
 }
